ad9cac077aa9210199e57b9408331e7bd6909ed4
Nagapramod Mandagere <pramod@us.ibm.com>
2017-02-20 15:37:02 -0800

    CI setup for PR build validation

---
+++ scripts/run_units.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "Setting up ginkgo and gomega"
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
+
+echo "Starting unit tests...."
+echo "No unit tests defined"
