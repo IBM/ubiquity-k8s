0991f91dbc3d9dc2506c3230a2bef78affbe46c2
Mohamed Mohamed <mmohamed@us.ibm.com>
2017-03-27 10:18:00 -0700

    Bump to latest ubiquity
    Signed-off-by: Nagapramod Mandagere <pramod@us.ibm.com>

--- vendor/github.ibm.com/almaden-containers/ubiquity/local/clients.go
+++ vendor/github.ibm.com/almaden-containers/ubiquity/local/clients.go
@@ -6,34 +6,25 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/gorm"
-	"github.ibm.com/almaden-containers/ubiquity/local/softlayer"
 	"github.ibm.com/almaden-containers/ubiquity/local/spectrumscale"
 	"github.ibm.com/almaden-containers/ubiquity/resources"
-	"github.ibm.com/almaden-containers/ubiquity/utils"
 )
 
-func GetLocalClients(logger *log.Logger, config resources.UbiquityServerConfig, database *gorm.DB, fileLock utils.FileLock) (map[resources.Backend]resources.StorageClient, error) {
+func GetLocalClients(logger *log.Logger, config resources.UbiquityServerConfig, database *gorm.DB) (map[resources.Backend]resources.StorageClient, error) {
 
 	clients := make(map[resources.Backend]resources.StorageClient)
-	spectrumClient, err := spectrumscale.NewSpectrumLocalClient(logger, config.SpectrumScaleConfig, database, fileLock)
+	spectrumClient, err := spectrumscale.NewSpectrumLocalClient(logger, config.SpectrumScaleConfig, database)
 	if err != nil {
 		logger.Printf("Not enough params to initialize '%s' client", resources.SPECTRUM_SCALE)
 	} else {
 		clients[resources.SPECTRUM_SCALE] = spectrumClient
 	}
 
-	spectrumNfsClient, err := spectrumscale.NewSpectrumNfsLocalClient(logger, config.SpectrumScaleConfig, database, fileLock)
+	spectrumNfsClient, err := spectrumscale.NewSpectrumNfsLocalClient(logger, config.SpectrumScaleConfig, database)
 	if err != nil {
 		logger.Printf("Not enough params to initialize '%s' client", resources.SPECTRUM_SCALE_NFS)
 	} else {
 		clients[resources.SPECTRUM_SCALE_NFS] = spectrumNfsClient
-	}
-
-	softlayerClient, err := softlayer.NewSoftlayerLocalClient(logger, config.SoftlayerConfig, database, fileLock)
-	if err != nil {
-		logger.Printf("Not enough params to initialize '%s' client", resources.SOFTLAYER_NFS)
-	} else {
-		clients[resources.SOFTLAYER_NFS] = softlayerClient
 	}
 
 	if len(clients) == 0 {
