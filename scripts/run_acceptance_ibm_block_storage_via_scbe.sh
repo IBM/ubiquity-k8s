9a13c35abb777fd70f90379e7830f63f2882d746
Shay Berman <shaybery@gmail.com>
2017-07-09 19:02:47 +0300

    #30 : minimal acceptance test for SCBE ubiquity backend

---
+++ scripts/run_acceptance_ibm_block_storage_via_scbe.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -ex
+
+scripts=$(dirname $0)
+
+echo "Creating Storage class...."
+kubectl create -f $scripts/../deploy/scbe_volume_storage_class.yml
+
+echo "Listing Storage classes"
+kubectl get storageclass
+
+
+echo "Creating Persistent Volume Claim..."
+kubectl create -f $scripts/../deploy/scbe_volume_pvc.yml
+
+
+echo "Listing Persistent Volume Claim..."
+kubectl get pvc
+
+
+echo "Listing Persistent Volume..."
+kubectl get pv
+
+
+echo "Creating Test Pod"
+kubectl create -f $scripts/../deploy/scbe_volume_with_pod.yml
+sleep 10
+
+echo "Listing pods"
+kubectl get pods
+
+echo "Writing success.txt to mounted volume"
+kubectl exec write-pod-test -c write-pod touch /mnt/success.txt
+
+echo "Reading from mounted volume"
+kubectl exec write-pod-test -c write-pod ls /mnt
+
+
+echo "Cleaning test environment"
+
+echo "Deleting Pod"
+kubectl delete -f $scripts/../deploy/scbe_volume_with_pod.yml
+
+echo "Deleting Persistent Volume Claim"
+kubectl delete -f $scripts/../deploy/scbe_volume_pvc.yml
+
+echo "Listing PVC"
+kubectl get pvc
+
+echo "Listing PV"
+kubectl get pv
+
+echo "Deleting Storage Class"
+kubectl delete -f $scripts/../deploy/scbe_volume_storage_class.yml
+
+echo "Listing Storage Classes"
+kubectl get storageclass
