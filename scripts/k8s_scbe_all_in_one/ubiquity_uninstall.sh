b6564a0cb34e7c2bdf3b38d6720b1f89d659078e
Shay Berman <shaybery@gmail.com>
2017-09-20 14:18:02 +0300

    #76 : uninstall - adding long timeout due to slow removal of PV and right ibm-ubiquity-db name

--- scripts/k8s_scbe_all_in_one/ubiquity_uninstall.sh
+++ scripts/k8s_scbe_all_in_one/ubiquity_uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 YML_DIR="./yamls"
-UBIQUITY_DB_PVC_NAME=ibm-ubiquity-database
+UBIQUITY_DB_PVC_NAME=ibm-ubiquity-db
 UTILS=../$scripts/acceptance_utils.sh
 
 [ ! -d "$YML_DIR" ] && { echo "Error: YML directory [$YML_DIR] does not exist."; exit 1; }
@@ -12,7 +12,7 @@ UTILS=../$scripts/acceptance_utils.sh
 pvname=`kubectl get pvc ${UBIQUITY_DB_PVC_NAME} --no-headers -o custom-columns=name:spec.volumeName`
 kubectl delete -f ${YML_DIR}/ubiquity-db-pvc.yml
 echo "Waiting for PVC ${UBIQUITY_DB_PVC_NAME} and PV $pvname to be deleted, before delete ubiquity and provisioner."
-wait_for_item_to_delete pvc ${UBIQUITY_DB_PVC_NAME} 10 2
+wait_for_item_to_delete pvc ${UBIQUITY_DB_PVC_NAME} 60 5  # TODO long timeout because an issue that cause the first PVC to be slow. should reduce it after the issue will be resolved
 wait_for_item_to_delete pv $pvname 10 2
 
 kubectl delete -f ${YML_DIR}/storage-class.yml
