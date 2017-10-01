01eff35bf338c532b2c452480754bafec3e11b07
Shay Berman <shaybery@gmail.com>
2017-10-01 22:56:16 +0300

    #90 : phase3 - improve daemon set, wait for object, texts, status and more. Details below
    Update in ubiquity_deployments.sh:
      1. status : Add indicate if missing element(but still not indicate if componenets up but not ok)
                  Add new arg getallwide
      2. start  : Add wait for ubiquity-db deployment and pod to started
    
    Update in ubiquity_install.sh:
      1. Hide kubectl get output on check
      2. Wait for ubiquity deployment
      3. Skip create configmap if already exist
      4. Wait for provisoner deployment
    
    Update in ubiquity_uninstall.sh:
      1. Remove the debug mode
      2. Add script description
      3. Add wait for ubiquity-db deployment to deletion
    
    In addition :
     1. Daemonset yml rename the deployment name to ubiquity-k8s-flex
     2. Update on all scripts :
    	license header
    	script descriptions update
            cosmetics

--- scripts/k8s_scbe_all_in_one/ubiquity_deployments.sh
+++ scripts/k8s_scbe_all_in_one/ubiquity_deployments.sh
@@ -1,20 +1,38 @@
 #!/bin/bash -e
 
+###################################################
+# Copyright 2017 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###################################################
+
 # -------------------------------------------------------------------------
 # The script start\stop ubiquity deployments with the right order.
 # The right order is to start ubiquity and provisioner before ubiquity-db and vise versa for stop.
 # The script assume that you already installed ubiquity via the ubiquity_install.sh script.
 #
 # Start flow order:
-#   - Validate ubiquity pvc exist (if not exit)
-#   - Create Ubiquity deployment
-#   - Create Ubiquity-k8s-provisioner deployment
-#   - Create ubiquity-db-deployment deployment
+#   1. Validate ubiquity pvc exist (exit if not)
+#   2. Create ubiquity deployment
+#   3. Create ubiquity-k8s-provisioner deployment
+#   4. Create ubiquity-k8s-flex Daemonset
+#   4. Create ubiquity-db-deployment deployment
 #
 # Start flow order:
-#   - Delete ubiquity-db-deployment deployment
-#   - Delete Ubiquity-k8s-provisioner deployment
-#   - Delete Ubiquity deployment
+#   1. Delete ubiquity-db-deployment deployment
+#   2. Delete Ubiquity-k8s-provisioner deployment
+#   3. Create ubiquity-k8s-flex Daemonset
+#   4. Delete Ubiquity deployment
 #
 # Assuming the following components was already created during the ubiquity_install.sh script.
 #   - Ubiquity service
@@ -24,17 +42,19 @@
 #   - PVC for the UbiquityDB
 #   - FlexVolume already installed on each minion and configured well
 #
+# Note : More details see usage below.
 # -------------------------------------------------------------------------
 
 
 function usage()
 {
-   echo "Usage, $0 [start|stop|status|statusall|help]"
+   echo "Usage, $0 [start|stop|status|getall|getallwide|help]"
    echo "    Options"
    echo "       start  : Create ubiquity, provisioner deployments, flex daemonset and ubiquity-db deployments"
    echo "       stop   : Delete ubiquity-db(wait for deletion), provisioner deployment, flex daemonset, ubiquity deployment"
    echo "       status : kubectl get to all the ubiquity components"
-   echo "       statusall : kubectl get configmap,storageclass,pv,pvc,service,daemonset,deployment,pod"
+   echo "       getall : kubectl get configmap,storageclass,pv,pvc,service,daemonset,deployment,pod"
+   echo "       getallwide : getall but with -o wide"
    echo "       help      : Show this usage"
    exit 1
 }
@@ -51,7 +71,6 @@ function start()
 
     kubectl create -f ${YML_DIR}/ubiquity-deployment.yml
     kubectl create -f ${YML_DIR}/ubiquity-k8s-provisioner-deployment.yml
-    sleep 5 # TODO wait for deployment
     kubectl create -f ${YML_DIR}/ubiquity-k8s-flex-daemonset.yml
     kubectl create -f ${YML_DIR}/ubiquity-db-deployment.yml
     echo "Finished to start ubiquity components. Run $0 status to get more details."
@@ -61,7 +80,10 @@ function stop()
 {
     # TODO delete the deployments only if its actually exist
     kubectl delete -f $YML_DIR/ubiquity-db-deployment.yml
-    sleep 30 # TODO wait till deployment stopped including the POD.
+    echo "Wait for ubiquity-db deployment deletion..."
+    wait_for_item_to_delete deployment ubiquity-db 10 4
+    wait_for_item_to_delete pod "ubiquity-db-" 10 4 regex  # to match the prefix of the pod
+
     kubectl delete -f $YML_DIR/ubiquity-k8s-provisioner-deployment.yml
     kubectl delete -f ${YML_DIR}/ubiquity-k8s-flex-daemonset.yml
     kubectl delete -f $YML_DIR/ubiquity-deployment.yml
@@ -71,19 +93,55 @@ function stop()
 
 function status()
 {
-    # kubectl get on configmap, storageclass, deployment and pod that related to ubiquity
-    kubectl get configmap k8s-config || :
+    # kubectl get on all the ubiquity components, if one of the components are not found
+    rc=0
+    kubectl get configmap k8s-config || rc=$?
     echo ""
-    kubectl get storageclass | egrep "ubiquity|^NAME"  || :
+    kubectl get storageclass | egrep "ubiquity|^NAME"  || rc=$?
     echo ""
-    kubectl get pv/ibm-ubiquity-db pvc/ibm-ubiquity-db svc/ubiquity svc/ubiquity-db  daemonset/ubiquity-flex deploy/ubiquity deploy/ubiquity-db deploy/ubiquity-k8s-provisioner  || :
+    kubectl get pv/ibm-ubiquity-db pvc/ibm-ubiquity-db svc/ubiquity svc/ubiquity-db  daemonset/ubiquity-k8s-flex deploy/ubiquity deploy/ubiquity-db deploy/ubiquity-k8s-provisioner  || rc=$?
     echo ""
-    kubectl get pod | egrep "^ubiquity|^NAME" || :
+    kubectl get pod | egrep "^ubiquity|^NAME" || rc=$?
+
+    if [ $rc != 0 ]; then
+       echo ""
+       echo "Ubiquity status [NOT ok]. Some components are missing(review the output above)"
+       exit 5
+    #else
+    #   # TODO verify deployment status
+    #   verify_deployments_status ubiquity ubiquity-db ubiquity-k8s-provisioner
+    fi
 }
 
-function statusall()
+function verify_deployments_status()
 {
-    kubectl get configmap,storageclass,pv,pvc,service,daemonset,deployment,pod || :
+       deployments="$@"
+       bad_deployment=""
+       for deployment_name in $deployments; do
+            if ! is_deployment_ok ${deployment_name}; then
+               echo "Deployment [${deployment_name}] status not OK"
+               bad_deployment="$deployment_name $bad_deployment"
+            fi
+       done
+       if [ -n "$bad_deployment" ]; then
+           echo ""
+           echo "Ubiquity status [NOT ok]. Some deployments are NOT ok (review the output above)."
+           exit 6
+       fi
+       # TODO also need to validate that the daemon set is in the current state
+
+       echo "Ubiquity status [OK]"
+       exit 0
+}
+function getall()
+{
+    flag="$1"
+    kubectl get $flag configmap,storageclass,pv,pvc,service,daemonset,deployment,pod || :
+}
+
+function getallwide()
+{
+    getall "-o wide"
 }
 
 
@@ -101,8 +159,7 @@ which kubectl > /dev/null 2>&1 || { echo "Error: kubectl not found in PATH"; exi
 . $UTILS # include utils for wait function and status
 [ $# -ne 1 ] && usage
 action=$1
-[ $action != "start" -a $action != "stop" -a $action != "status" -a $action != "statusall" -a $action != "help" ] && usage
-
+[ $action != "start" -a $action != "stop" -a $action != "status" -a $action != "getall" -a $action != "getallwide" -a $action != "help" ] && usage
 
 
 $1
--- scripts/k8s_scbe_all_in_one/ubiquity_install.sh
+++ scripts/k8s_scbe_all_in_one/ubiquity_install.sh
@@ -1,51 +1,47 @@
 #!/bin/bash -e
 
+###################################################
+# Copyright 2017 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###################################################
+
 # -------------------------------------------------------------------------
 # The script install all Ubiquity components inside kubernetes(k8s) cluster.
 #
 # Create the following components:
-#   - Ubiquity service
-#   - Ubiquity-db service
-#   - Ubiquity deployment
-#   - configmap k8s-config for Ubiquity-k8s-provisioner
-#   - Ubiquity-k8s-provisioner deployment
-#   - Storage class that match for the UbiquityDB PVC
-#   - PVC for the UbiquityDB
-#   - Update all minions with the Ubiquity service IP # TODO will be change to daemon set later on
-#   - TODO : create postgres deployment
+#   1. Ubiquity service
+#   2. Ubiquity-db service
+#   3. Ubiquity deployment
+#   4. configmap k8s-config for Ubiquity-k8s-provisioner  (if already exist then skip creation)
+#   5. Ubiquity-k8s-provisioner deployment
+#   6. Storage class that match for the UbiquityDB PVC
+#   7. PVC for the UbiquityDB
+#   8. ubiquity-k8s-flex DaemonSet (deploy the flex + flex config file to all nodes)
+#   9. optional : ubiquity-db deployment (only if -d flag specify. Use it only if the flex already deployed in the past on the nodes,
+#                 therefor no restart for kubelet is needed before run the deployment)
 #
 # Prerequisites to run this test:
 #   - The script assumes all the yamls exist under ./yamls and updated with relevant configuration.
 #     If config file (with placeholder KEY=VALUE) given as argument then the script will apply all the place holders inside all the yaml files.
-#   - The script assumes the customer update all the ymls with relevant configurations.
 #   - Run the script in the Kubernetes master node where you have access to kubectl command.
-#   - KUBECONF environment should set to the k8s config file that will be loaded as configmap for the ubiquity provisioner.
+#   - See usage function below for more details about flags.
 # -------------------------------------------------------------------------
-
-function update_ymls_with_playholders()
-{
-   placeholder_file="$1"
-   [ -z "${placeholder_file}" ] && return
-
-   [ ! -f "${placeholder_file}" ] && { echo "Error : ${placeholder_file} not found."; exit 3; }
-   read -p "Update ymls with placeholders. Are you sure (y/n): " yn
-   if [ "$yn" = "y" ]; then
-       for line in `cat ${placeholder_file}`; do
-          echo "   $line"
-          placeholder=`echo $line | awk -F= '{print $1}'`
-          value=`echo $line | awk -F= '{print $2}'`
-          sed -i "s|${placeholder}|${value}|g" "$YML_DIR"/*.yml
-       done
-       echo "Finish to update yaml according to ${config_keys}"
-   else
-      echo "Running installation without update ymls with placeholder."
-   fi
-}
 
 function usage()
 {
   cmd=`basename $0`
-  echo "USAGE, $cmd [-d] [-c filepath] [-k filepath] [-h]"
+  echo "USAGE   $cmd [-d] [-c filepath] [-k filepath] [-h]"
   echo "  Options:"
   echo "    -d            : Assume flex plugin in already loaded so create ubiquity-db deployment after daemon-set"
   echo "    -c [filepath] : if given, then first sed all key-value in filepath on all yaml directory"
@@ -54,13 +50,34 @@ function usage()
   exit 1
 }
 
+function update_ymls_with_playholders()
+{
+   placeholder_file="$1"
+   [ -z "${placeholder_file}" ] && return
+
+   [ ! -f "${placeholder_file}" ] && { echo "Error : ${placeholder_file} not found."; exit 3; }
+   read -p "Updating ymls with placeholders from ${placeholder_file} file. Are you sure (y/n): " yn
+   if [ "$yn" = "y" ]; then
+       for line in `cat ${placeholder_file}`; do
+          echo "   $line"
+          placeholder=`echo $line | awk -F= '{print $1}'`
+          value=`echo $line | awk -F= '{print $2}'`
+          sed -i "s|${placeholder}|${value}|g" "$YML_DIR"/*.yml
+       done
+       echo "Finish to update yaml according to ${placeholder_file}"
+   else
+      echo "Running installation without update ymls with placeholder."
+   fi
+}
+
+# Variables
 scripts=$(dirname $0)
 YML_DIR="$scripts/yamls"
 UTILS=$scripts/ubiquity_utils.sh
 UBIQUITY_DB_PVC_NAME=ibm-ubiquity-db
-FLEX_DIRECTORY='/usr/libexec/kubernetes/kubelet-plugins/volume/exec/ibm~ubiquity-k8s-flex'
+K8S_CONFIGMAP_FOR_PROVISIONER=k8s-config
 
-# Handle flags to the installer
+# Handle flags
 to_deploy_ubiquity_db="false"
 KUBECONF=~/.kube/config
 CONFIG_SED_FILE=""
@@ -91,13 +108,13 @@ done
 
 # Validations
 [ ! -d "$YML_DIR" ] && { echo "Error: YML directory [$YML_DIR] does not exist."; exit 1; }
-which kubectl || { echo "Error: kubectl not found in PATH"; exit 2; }
+which kubectl > /dev/null 2>&1 || { echo "Error: kubectl not found in PATH"; exit 2; }
 [ ! -f $UTILS ] && { echo "Error: $UTILS file not found"; exit 3; }
 [ ! -f "$KUBECONF" ] && { echo "$KUBECONF not found"; exit 2; }
 [ -n "$CONFIG_SED_FILE" -a ! -f "$CONFIG_SED_FILE" ] && { echo "$CONFIG_SED_FILE not found"; exit 2; }
 . $UTILS # include utils for wait function and status
 
-
+# update yamls if -c flag given
 [ -n "${CONFIG_SED_FILE}" ] && update_ymls_with_playholders ${CONFIG_SED_FILE}
 
 # Start to create ubiquity components in order
@@ -106,25 +123,27 @@ which kubectl || { echo "Error: kubectl not found in PATH"; exit 2; }
 
 kubectl create -f ${YML_DIR}/ubiquity-service.yml
 kubectl create -f ${YML_DIR}/ubiquity-db-service.yml
-
 kubectl create -f ${YML_DIR}/ubiquity-deployment.yml
-sleep 2 # TODO wait for deployment
+wait_for_deployment ubiquity 5 5
 
-kubectl create configmap k8s-config --from-file $KUBECONF   # TODO consider use secret or assume customer set it up in advance
-sleep 2 # TODO wait for deployment
-
+if ! kubectl get cm/${K8S_CONFIGMAP_FOR_PROVISIONER} > /dev/null 2>&1; then
+    kubectl create configmap ${K8S_CONFIGMAP_FOR_PROVISIONER} --from-file $KUBECONF
+else
+    echo "Skip the creation of ${K8S_CONFIGMAP_FOR_PROVISIONER} configmap, because its already exist"
+fi
 kubectl create -f ${YML_DIR}/ubiquity-k8s-provisioner-deployment.yml
+wait_for_deployment ubiquity-k8s-provisioner 5 5
 
+# Create Storage class and PVC, then wait for PVC and PV creation
 kubectl create -f ${YML_DIR}/storage-class.yml
-
 kubectl create -f ${YML_DIR}/ubiquity-db-pvc.yml
-
 echo "Waiting for ${UBIQUITY_DB_PVC_NAME} PVC to be created"
 wait_for_item pvc ${UBIQUITY_DB_PVC_NAME} ${PVC_GOOD_STATUS} 60 5
 pvname=`kubectl get pvc ${UBIQUITY_DB_PVC_NAME} --no-headers -o custom-columns=name:spec.volumeName`
 echo "Waiting for ${pvname} PV to be created"
 wait_for_item pv $pvname ${PVC_GOOD_STATUS} 20 3
 
+# Create ${flex_conf} configmap with the ubiquity-service clusterIP
 ubiquity_service_ip=`kubectl get svc/ubiquity -o=custom-columns=:.spec.clusterIP | tail -1`
 echo "Deploy flex driver as infinit daemonset, Its also copy the flex config file with the ubiquity service IP [$ubiquity_service_ip]"
 flex_conf="ubiquity-k8s-flex.conf"
@@ -134,17 +153,17 @@ kubectl create configmap ${flex_conf} --from-file ${YML_DIR}/${flex_conf}
 kubectl create -f ${YML_DIR}/ubiquity-k8s-flex-daemonset.yml
 
 if [ "${to_deploy_ubiquity_db}" == "true" ]; then
-    echo "Assume flex plugin in already loaded in minions, lets create the ubiquity-db deployment..."
+    echo "Creating ubiquity-db deployment... (Assume flex plugin was already loaded on all the nodes)"
     kubectl create -f ${YML_DIR}/ubiquity-db-deployment.yml
+    echo "Waiting for deployment [ubiquity-db] to be created..."
     wait_for_deployment ubiquity-db 40 5
 
     echo ""
-    echo "Finished to install Ubiquity components inside the Kubernetes cluster."
-    echo "  To list ubiquity deployments :$> ubiquity_deployments.sh status"
+    echo "Ubiquity installation finished successfully in the Kubernetes cluster. (To list ubiquity deployments run $> ./ubiquity_deployments.sh status)"
 else
     echo ""
-    echo "Finished, but ubiquity not ready yet."
-    echo " You must do : (1) Manually restart kubelet service on all minions to reload the new flex driver"
-    echo "               (2) $> kubectl create -f ubiquity-db-deployment.yml"
-    echo ""
+    echo "Ubiquity installation finished, but Ubiquity is NOT ready yet."
+    echo "  You must do : (1) Manually restart kubelet service on all minions to reload the new flex driver"
+    echo "                (2) $> kubectl create -f ${YML_DIR}/ubiquity-db-deployment.yml"
 fi
+echo ""
