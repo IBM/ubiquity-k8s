d1e351384cf6308b81159b581f6dd1f2e688aebc
hfeish <hfeish@cn.ibm.com>
2017-11-07 03:50:56 -0500

    increase size and time for product env

--- deploy/k8s_deployments/setup_flex.sh
+++ deploy/k8s_deployments/setup_flex.sh
@@ -70,7 +70,7 @@ while : ; do
   # Run a tail -f on the flex log file (which locate on the host), so it will be visible by running kubectl logs <flex POD>
   # TODO change to the new location later on : /usr/libexec/kubernetes/kubelet-plugins/volume/exec/ibm~ubiquity-k8s-flex/ubiquity-k8s-flex.log &
   tail -F /var/tmp/ubiquity-k8s-flex.log &
-  sleep 300 # every 24 hours
+  sleep 86400 # every 24 hours
   echo "Start ubiquity logrotate"
   /usr/sbin/logrotate /etc/logrotate.d/ubiquity_logrotate
   echo "Finish ubiquity logrotate"
